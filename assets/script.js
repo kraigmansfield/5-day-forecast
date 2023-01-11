85b734ec4d7a154606fb4912a4f53bc569406669
kraigmansfield <Kraig.mansfield@gmail.com>
2023-01-10 17:32:58 -0900

    updated js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,22 @@
-let weather = {
-    apiKey : "e07f73fd554a9ab1da8c6467b8557d53",
-    fetchWeather: function(city){
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=e07f73fd554a9ab1da8c6467b8557d53')
-            .then((response) => response.json())
-            .then((data) => console.log(data))
+function getInfo(){
+    var newName= document.querySelector("cityInput");
+    var cityName = document.querySelector("cityName");
+}
 
-    }}
+function searchWeather(city){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=' +${newName}+'&units=metric&appid=e07f73fd554a9ab1da8c6467b8557d53`)
+    .then(response => response.json(){
+        return res.json(); 
+    }).then(function(data){
+        renderResults(data)
+    })}
 
-weather.fetchWeather();
+    searchWeather();
 
+
+searchForm.addEventListener("submit",function(e){
+    e.preventDefault();
+    var cityToSearch = searchCityInput.value;
+    searchWeather(city);
+
+})
