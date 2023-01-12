8797be1bd7d982e12b3b97e471ee5f3466d3bce5
kraigmansfield <Kraig.mansfield@gmail.com>
2023-01-11 19:23:25 -0900

    updates

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,43 @@
 
 var currentLocation = document.getElementsByClassName('todays-stats')[0];
 var currentHigh = document.getElementById('today-high')[0];
-var currentLow = document.getElementById('today-low')[0];
 var currentWind = document.getElementById('today-wind')[0];
 var currentHumidity = document.getElementById('today-humidity')[0];
+var searchFormInput = document.querySelector("#search-term")
+var searchForm = document.querySelector("search-btn")
+var currentDate = (new Date()).toLocaleDateString('en-US')
 
 
 
 
+function searchWeather(city){   
+  
+  var city = document.getElementById("city".value);
 
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e07f73fd554a9ab1da8c6467b8557d53`)
+  .then(function(res){
+    return res.json();
+  }).then(function(data){
+    //appending to page
+    console.log(data)
+    var cityInput = document.getElementById("city".value);
+    var cityName = (data.name);
+    var high = (data.main.temp);
+    var wind = (data.main.wind.speed);
+    var humidity = (data.main.humidity);
 
-    requestURL = `https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=e07f73fd554a9ab1da8c6467b8557d53`
-    fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      ;
-        for (let i = 0; i < data.length; i++) {
-            console.log(data);
-    }
-
-renderData = (location, forecast) => {
-    var currentWeather = forcast[0].weather[0];
     
+  })
 }
+
+searchWeather("seattle");
+
+
+
 
 searchForm.addEventListener("submit",function(e){
     e.preventDefault();
-    var cityToSearch = cityName.value;
-    searchWeather(newName);
+    var cityToSearch = searchFormInput.value;
+    console.log(cityToSearch)
 
-})})
+})
